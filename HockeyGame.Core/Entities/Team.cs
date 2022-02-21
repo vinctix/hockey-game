8afd96a20762156830bd1c0c1e25f844af2ebb96
Vincent TIXIER <vtixier@werecruit.io>
2022-02-21 13:00:20 +0100

    Suppress some warnings about nullable fields

--- HockeyGame.Core/Entities/Team.cs
+++ HockeyGame.Core/Entities/Team.cs
@@ -6,36 +6,24 @@ namespace HockeyGame.Core.Entities
     public class Team
     {
         [Key] public int Id { get; set; }
-        public string Coach { get; set; }
+        public string? Coach { get; set; }
         public int Year { get; set; }
-        public IEnumerable<Player> Players { get; set; }
+        public IEnumerable<Player>? Players { get; set; }
 
         [NotMapped]
         public Player? Captain
         {
-            get { return Players.FirstOrDefault(x => x.IsCaptain); }
+            get { return Players?.FirstOrDefault(x => x.IsCaptain); }
             set
             {
-                foreach (var player in Players)
+                if (Players != null)
                 {
-                    player.IsCaptain = player.Id == value?.Id;
+                    foreach (var player in Players)
+                    {
+                        player.IsCaptain = player.Id == value?.Id;
+                    }
                 }
             }
         }
-
-        // public Player? GetCaptain()
-        // {
-        //     return Players.FirstOrDefault(x => x.IsCaptain);
-        // }
-        //
-        // public Player SetCaptain(Player captain)
-        // {
-        //     foreach (var player in Players)
-        //     {
-        //         player.IsCaptain = player.Id == captain.Id;
-        //     }
-        //
-        //     return captain;
-        // }
     }
 }
